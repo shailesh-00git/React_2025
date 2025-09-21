9927232b5057ce277d482a83f4c427b369ad1ade
shailesh-00git <sy468698@gmail.com>
2025-09-21 22:35:17 +0545

    Added render the root componenet and struct mode

---
+++ 04- react basic/rendering the root comp and strict mode/render/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+function App() {
+  return <h1>Hello I am person world!!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
