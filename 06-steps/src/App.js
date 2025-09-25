911834b9f0676d4c0f23d2eff656876c6d40ce72
shailesh-00git <sy468698@gmail.com>
2025-09-25 23:43:44 +0545

    Added the 06-step, with react states, events and eventListeners

---
+++ 06-steps/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+const messages = [
+  "Learn react 😎",
+  "Seek for job👌 ",
+  "Earn money and live happily ❤️",
+];
+
+function App() {
+  const [step, setStep] = React.useState(1);
+  function handleNext() {
+    if (step < 3) setStep(step + 1);
+  }
+  function handlePrevious() {
+    if (step > 1) setStep(step - 1);
+  }
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step === 1 ? "active" : ""}`}>1</div>
+        <div className={`${step === 2 ? "active" : ""}`}>2</div>
+        <div className={`${step === 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className="message">
+        step :{step} {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "purple", color: "white" }}
+          onClick={() => handlePrevious()}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "purple", color: "white" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+export default App;
